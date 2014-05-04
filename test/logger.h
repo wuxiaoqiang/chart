6c19caee606fe3a5f3ac8fc4a45b1b2ec3c2c602
wujianguo <github@wujianguo.org>
2014-05-04 10:15:46 +0800

    logger

--- test/logger.h
+++ test/logger.h
@@ -1,32 +1,17 @@
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
-// #include <stdio.h>
 
 void log_init(char* cfg_path);
-// void log_ex(int level, const char* module, const char* func, unsigned int line, const char* fmt, ...);
 void log_ex(int level, int module, const char* func, unsigned int line, const char* fmt, ...);
-// void log_ex(const char* fmt, ...);
 void log_uninit();
-
-// #define log_ex printf
-// #define LOG_DEBUG(format, ...) printf("[%d:%s:%d] " format "\n", gettid(),__FUNCTION__, __LINE__,##__VA_ARGS__)
-// #define LOG_ERROR(format, ...) printf("[%d:%s:%d] " format "\n", gettid(),__FUNCTION__, __LINE__,##__VA_ARGS__)
 
 #ifndef LOG_MODULE
 #error Compile error, must define LOG_MODULE
 #endif
 
-// #define LEVEL_DEBUG 0
-// #define LEVEL_ERROR 1
+#define LOG_DEBUG(format, ...) log_ex(LOG_LEVEL_DEBUG, LOG_MODULE, __FUNCTION__, __LINE__, format "\n", ##__VA_ARGS__)
 
-// #define LOG_DEBUG(format, ...) log_ex("[debug][" LOG_MODULE "][%d][%s:%d] " format "\n", gettid(),__FUNCTION__,__LINE__,##__VA_ARGS__);
-// #define LOG_DEBUG(format, ...) log_ex(LEVEL_DEBUG, ppp[LOG_MODULE], __FUNCTION__, __LINE__, format "\n", ##__VA_ARGS__);
-
-// #define LOG_ERROR(format, ...) log_ex(LEVEL_ERROR, ppp[LOG_MODULE], __FUNCTION__, __LINE__, format "\n", ##__VA_ARGS__);
-
-#define LOG_DEBUG(format, ...) log_ex(LOG_LEVEL_DEBUG, LOG_MODULE, __FUNCTION__, __LINE__, format "\n", ##__VA_ARGS__);
-
-#define LOG_ERROR(format, ...) log_ex(LOG_LEVEL_ERROR, LOG_MODULE, __FUNCTION__, __LINE__, format "\n", ##__VA_ARGS__);
+#define LOG_ERROR(format, ...) log_ex(LOG_LEVEL_ERROR, LOG_MODULE, __FUNCTION__, __LINE__, format "\n", ##__VA_ARGS__)
 
 #include <android/log.h>
 
@@ -36,17 +21,17 @@ void log_uninit();
 #define STD_LOG_ERROR(format, ...) __android_log_print(ANDROID_LOG_ERROR  , "downloadengine", "[%s:%d] " format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 
+#define LOGID_MAP(XX)                                 \
+  XX(ASYNC_FRAME, "async_frame")                      \
+  XX(MOVIE_MANAGER, "movie_manager")                  \
+  XX(DISPATCHER, "dispatcher")                        \
+  XX(CACHE, "cache")                                  \
+  XX(TASK_MANAGER, "task_manager")                    \
 
-/* Expand this list if necessary. */
-#define LOGID_MAP(XX)										\
-  XX(ASYNC_FRAME, "async_frame")                    	\
-  XX(MOVIE_MANAGER, "movie_manager")                    \
-  XX(TASK_MANAGER, "task_manager")                 		\
 
-
-#define LOG_LEVEL_MAP(XX)									\
-  XX(LEVEL_DEBUG, "debug")									\
-  XX(LEVEL_ERROR, "error")									\
+#define LOG_LEVEL_MAP(XX)                             \
+  XX(LEVEL_DEBUG, "debug")                            \
+  XX(LEVEL_ERROR, "error")                            \
 
 typedef enum {
   // UV_UNKNdOWN_HANDLE = 0,
@@ -58,9 +43,9 @@ typedef enum {
 
 typedef enum {
 #define XX(uc, lc) LOG_##uc,
-	LOG_LEVEL_MAP(XX)
+  LOG_LEVEL_MAP(XX)
 #undef XX
-	LOG_LEVEL_OFF
+  LOG_LEVEL_OFF
 }log_levles;
 
 #endif
